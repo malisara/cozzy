03124287a218afb2ad6c68e17f078fadf9b2191f
Sara Mali <mali.sara7@gmail.com>
2023-07-13 13:29:27 +0200

    hero I

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Hero from "@/components/hero/Hero";
+
 export default function Home() {
-  return <main className="mt-16">page</main>;
+  return (
+    <main className="mt-16">
+      <Hero />
+    </main>
+  );
 }
---
+++ src/components/hero/heroData.ts
@@ -0,0 +1,24 @@
+import HeroMen from "../../../public/hero-men.png";
+import HeroWomen from "../../../public/hero-women.png";
+import HeroJewelry from "../../../public/hero-jewelry.png";
+
+export const heroData = [
+  {
+    id: 1,
+    src: HeroWomen,
+    alt: "women's clothes",
+    title: "Comfort",
+  },
+  {
+    id: 2,
+    src: HeroMen,
+    alt: "men's clothes",
+    title: "Elegance",
+  },
+  {
+    id: 3,
+    src: HeroJewelry,
+    alt: "jewelry",
+    title: "Timeless peaces",
+  },
+];
