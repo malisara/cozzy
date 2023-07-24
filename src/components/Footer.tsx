034c1f03503f07b7beef3eedec358660a86d6dbe
Sara Mali <mali.sara7@gmail.com>
2023-07-24 14:43:17 +0200

    footer html change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 function Footer(): JSX.Element {
   return (
-    <div
+    <footer
       className="flex fixed bottom-0 w-full h-16 justify-center
      items-center bg-stone-50 drop-shadow-lg"
     >
       copyright © Cozzy 2023{" "}
-    </div>
+    </footer>
   );
 }
 
