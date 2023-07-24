5d0f5801975e196f3b4a3e35e497597304bdf8b8
Sara Mali <mali.sara7@gmail.com>
2023-07-24 15:51:03 +0200

    banner text

---
+++ src/components/bannerText/BannerText.tsx
@@ -0,0 +1,18 @@
+function BannerText(): JSX.Element {
+  return (
+    <div
+      className="flex justify-center text-center flex-col relative 
+       bg-gray-800 mt-[20rem] lg:mt-[50rem] h-[10rem] px-6 py-[6rem] 
+       lg:py-[10rem] content-center"
+    >
+      <h2 className="text-2xl lg:text-4xl font-bold text-base-secondary">
+        Cozzy Up Your Wardrobe Today!
+      </h2>
+      <p className="mt-4 lg:mt-10 text-md lg:text-xl">
+        Elevate your style with high-quality fashion in stunning colors.
+      </p>
+    </div>
+  );
+}
+
+export default BannerText;
