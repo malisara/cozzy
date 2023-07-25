3d89fcb96fda9c112df331e25c15559853866236
Sara Mali <mali.sara7@gmail.com>
2023-07-25 19:53:39 +0200

    refactoring III

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+import Loading from "@/components/loading/Loading";
+
+function loading(): JSX.Element {
+  return <Loading />;
+}
+
+export default loading;
--- src/components/allItems/AllItems.tsx
+++ src/components/allItems/AllItems.tsx
@@ -4,11 +4,33 @@ import { useEffect, useState } from "react";
 import { BACKEND_API_URL, LOCAL_STORAGE_KEY } from "@/constants";
 import Item from "@/models/item";
 import SingleItem from "../singleItem/SingleItem";
+import useSWR from "swr";
+import NoItems from "@/components/noItems/NoItems";
+import Loading from "@/components/loading/Loading";
+
+async function fetcher(category: string, limit?: number): Promise<Item[]> {
+  let apiUrl = `${BACKEND_API_URL}/products/category/${category}`;
+  if (limit) {
+    apiUrl += `?limit=${limit}`;
+  }
+  return await fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      return data.map((itemObj: any) => {
+        return new Item(
+          itemObj.id,
+          itemObj.title,
+          itemObj.price,
+          itemObj.image
+        );
+      });
+    });
+}
 
 type Props = {
   gridDisplay: boolean;
   sortPriceDescending: boolean;
-  category?: string;
+  category: string;
   limit?: number;
 };
 
@@ -21,41 +43,37 @@ function AllItems({
   const lineDisplayStyle = "flex-nowrap overflow-x-auto"; // TODO
   const gridDisplayStyle = "flex-wrap justify-center";
   const [itemsToDisplay, setItemsToDisplay] = useState<Item[]>([]);
-  const [savedItems, setSavedItems] = useState<string[]>(
-    JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || "[]")
+  const [savedItems, setSavedItems] = useState<string[]>([]);
+
+  const { data, error, isLoading } = useSWR(category, () =>
+    fetcher(category, limit)
   );
 
   useEffect(() => {
-    let apiUrl = `${BACKEND_API_URL}/products`;
-
-    if (category) {
-      apiUrl += `/category/${category}`;
-    }
-
-    if (limit) {
-      apiUrl += `?limit=${limit}`;
-    }
-
-    fetch(`${apiUrl}`)
-      .then((response) => response.json())
-      .then((data) => {
-        const items: Item[] = data.map((itemObj: any) => {
-          return new Item(
-            itemObj.id,
-            itemObj.title,
-            itemObj.price,
-            itemObj.image
-          );
-        });
-
-        setItemsToDisplay(sortItems(items));
-      });
+    //LS not defined on a window object
+    const savedItems = JSON.parse(
+      localStorage.getItem(LOCAL_STORAGE_KEY) || "[]"
+    );
+    setSavedItems(savedItems);
   }, []);
+
+  useEffect(() => {
+    if (data) {
+      setItemsToDisplay(sortItems(data));
+    }
+  }, [data]);
 
   useEffect(() => {
     const items = [...itemsToDisplay];
     setItemsToDisplay(sortItems(items));
   }, [sortPriceDescending]);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(savedItems));
+  }, [savedItems]);
+
+  if (error) return <NoItems />;
+  if (isLoading || !data) return <Loading />;
 
   function sortItems(items: Item[]): Item[] {
     if (sortPriceDescending === true) {
@@ -74,17 +92,13 @@ function AllItems({
     }
   }
 
-  useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(savedItems));
-  }, [savedItems]);
-
   return (
     <div
       className={`${
         gridDisplay ? gridDisplayStyle : lineDisplayStyle
       } flex gap-[5rem]`}
     >
-      {itemsToDisplay.map((item: any) => {
+      {itemsToDisplay.map((item: Item) => {
         return (
           <SingleItem
             item={item}
