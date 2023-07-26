26328449c36d93404fdc66a907ae9434611d3315
Sara Mali <mali.sara7@gmail.com>
2023-07-26 16:58:24 +0200

    button component

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+type Props = {
+  text: string;
+  disabled?: boolean;
+};
+
+function Button({ text, disabled }: Props): JSX.Element {
+  const btnStyle = disabled
+    ? "bg-gray-300"
+    : "border-base-secondary bg-base-secondary hover:opacity-75 rounded-md transtion-all duration-500";
+
+  return (
+    <button
+      disabled={disabled}
+      className={`${btnStyle} border px-3 py-2 text-white`}
+    >
+      {text}
+    </button>
+  );
+}
+
+export default Button;
