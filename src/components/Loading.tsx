0ee9847dbd0afdfd9bfcd86c53840a2fe7d5fa85
Sara Mali <mali.sara7@gmail.com>
2023-08-14 17:25:24 +0200

    loading component update

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -4,7 +4,7 @@ import { flexCenter } from "./utils/style";
 
 function Loading(): JSX.Element {
   return (
-    <div className={`${flexCenter} mt-[10rem] lg:mt-[15rem]`}>
+    <div className={`${flexCenter}  mt-[30vh] lg:mt-[40vh]`}>
       <PiCoatHanger
         className="animate-spin text-[7rem]
        text-base-secondary"
