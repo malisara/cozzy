56da641a6ddb088f648b747df6f8d1d1b5f26cec
Sara Mali <mali.sara7@gmail.com>
2023-08-16 19:45:17 +0200

    constants

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,7 @@ export const DEFAULT_HOMEPAGE_CATEGORY = "women's clothing";
 export const LOCAL_STORAGE_KEY = "saved_items";
 export const BACKEND_API_URL = "https://fakestoreapi.com";
 export const ITEM_DISPLAY_LIMIT = 5;
+
+//current user id & shopping bag id
+export const USERT_ID = 3;
+export const SHOPPING_BAG_NUMBER = 5;
