bde9184d9d46d33e2230bd7cf01f293c154c3dd7
Sara Mali <mali.sara7@gmail.com>
2023-08-25 13:11:53 +0200

    btn style constant

--- src/components/utils/style.ts
+++ src/components/utils/style.ts
@@ -1,3 +1,5 @@
 export const flexCenter = "flex items-center justify-center";
-
 export const imageCover = "object-cover w-full h-full";
+export const wideBtnStyle =
+  "border-base-secondary bg-base-secondary hover:opacity-75 rounded-md\
+   transtion-all duration-500 text-white px-4 py-2 w-full mt-5 text-xl";
