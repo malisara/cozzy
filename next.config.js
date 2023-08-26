d74e142d8b021b74053736e8470335456d3c0627
Sara Mali <mali.sara7@gmail.com>
2023-08-26 22:08:11 +0200

    build settings

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-module.exports = nextConfig;
 module.exports = {
+  // output: "export",
+
   images: {
     remotePatterns: [
       {
